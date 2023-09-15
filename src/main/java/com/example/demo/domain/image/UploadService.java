de4e391aa8de32b777e612d6cae20bc9700cde5e
이언석 <djstjr14@naver.com>
2023-09-15 17:22:08 +0900

    firebase

---
+++ src/main/java/com/example/demo/domain/image/UploadService.java
@@ -0,0 +1,9 @@
+package com.example.demo.domain.charge;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChargeService {
+}
